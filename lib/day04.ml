2cc381ca977e6236372a635b12ce20a8ee42fb8a
Rascat <rascat@mailbox.org>
2023-12-04 21:18:12 +0100

    Refactor day04.ml using Jane Streets base library

--- lib/day04.ml
+++ lib/day04.ml
@@ -1,3 +1,5 @@
+open Base
+
 type card =
   { id : int
   ; winning_numbers : int list
@@ -5,68 +7,76 @@ type card =
   }
 
 let parse_card line =
-  let colon_index = String.index line ':' in
-  let id = Scanf.sscanf line "Card %d:" (fun id -> id) in
-  let numbers_part =
-    String.sub line (colon_index + 1) (String.length line - colon_index - 1)
+  let colon_index =
+    match String.index line ':' with
+    | Some i -> i
+    | None -> failwith "No colon found"
   in
-  let winner_own_parts = String.split_on_char '|' numbers_part in
+  let id = Stdlib.Scanf.sscanf line "Card %d:" (fun id -> id) in
+  let numbers_part =
+    String.sub line ~pos:(colon_index + 1) ~len:(String.length line - colon_index - 1)
+  in
+  let winner_own_parts = String.split ~on:'|' numbers_part in
   let winning_numbers =
-    String.split_on_char ' ' (List.nth winner_own_parts 0)
-    |> List.filter (fun x -> String.length x > 0)
-    |> List.map int_of_string
+    String.split
+      (match List.nth winner_own_parts 0 with
+       | Some x -> x
+       | None -> failwith "No winning numbers")
+      ~on:' '
+    |> List.filter ~f:(fun x -> String.length x > 0)
+    |> List.map ~f:Int.of_string
   in
   let own_numbers =
-    String.split_on_char ' ' (List.nth winner_own_parts 1)
-    |> List.filter (fun x -> String.length x > 0)
-    |> List.map int_of_string
+    String.split
+      ~on:' '
+      (match List.nth winner_own_parts 1 with
+       | Some x -> x
+       | None -> failwith "No own numbers")
+    |> List.filter ~f:(fun x -> String.length x > 0)
+    |> List.map ~f:Int.of_string
   in
   { id; winning_numbers; own_numbers }
 ;;
 
 let count_matching_numbers winning_numbers own_numbers =
-  List.fold_left
-    (fun acc number -> if List.mem number winning_numbers then acc + 1 else acc)
-    0
+  List.fold
     own_numbers
+    ~f:(fun acc number ->
+      if List.mem winning_numbers number ~equal:(fun x y -> x = y) then acc + 1 else acc)
+    ~init:0
 ;;
 
 let compute_card_score num_matches =
-  Float.to_int
-    (if num_matches >= 1 then Float.pow 2.0 (float_of_int (num_matches - 1)) else 0.0)
+  if num_matches >= 1 then 2 ** (num_matches - 1) else 0
+;;
+
+let solve_part_one data =
+  List.map ~f:parse_card data
+  |> List.map ~f:(fun card ->
+    count_matching_numbers card.winning_numbers card.own_numbers)
+  |> List.map ~f:compute_card_score
+  |> Utils.sum
 ;;
 
 let rec inc_next_n_by curr_index n inc deck =
   if n = 0
   then deck
   else (
-    let next_index = (curr_index + 1) mod Array.length deck in
+    let next_index = (curr_index + 1) % Array.length deck in
     deck.(next_index) <- deck.(next_index) + inc;
     inc_next_n_by next_index (n - 1) inc deck)
 ;;
 
-let solve_part_one _data =
-  let cards = _data |> List.map parse_card in
-  List.map
-    (fun card -> count_matching_numbers card.winning_numbers card.own_numbers)
-    cards
-  |> List.map compute_card_score
-  |> Utils.sum
-;;
-
-let solve_part_two _data =
-  let cards = _data |> List.map parse_card in
-  let deck = Array.make (List.length cards) 1 in
+let solve_part_two data =
+  let cards = data |> List.map ~f:parse_card in
+  let deck = Array.init (List.length cards) ~f:(fun _ -> 1) in
   let rec loop cards deck =
     match cards with
     | [] -> deck
     | card :: rest ->
       let num_matches = count_matching_numbers card.winning_numbers card.own_numbers in
       let curr_increment = deck.(card.id - 1) in
-      (* pretty print deck *)
-      Array.iter (fun x -> Printf.printf "%d " x) deck;
-      Printf.printf "\n";
       loop rest (inc_next_n_by (card.id - 1) num_matches curr_increment deck)
   in
-  loop cards deck |> Array.fold_left ( + ) 0
+  loop cards deck |> Array.fold ~f:( + ) ~init:0
 ;;
