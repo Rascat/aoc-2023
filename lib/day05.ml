1712dbfb72458be7eaade2d00f6a4ec0cd0333c1
Rascat <rascat@mailbox.org>
2023-12-05 15:38:50 +0100

    Submit code for solving day 5.1

---
+++ lib/day05.ml
@@ -0,0 +1,109 @@
+type range =
+  { destination_range_start : int
+  ; source_range_start : int
+  ; length : int
+  }
+
+type map =
+  { name : string
+  ; ranges : range list
+  }
+
+let parse_maps input =
+  let is_new_map line = String.contains line ':' in
+  let is_range line =
+    Re.Str.string_match (Re.Str.regexp "\\([0-9]+\\) \\([0-9]+\\) \\([0-9]+\\)") line 0
+  in
+  let is_empty line = String.length line = 0 in
+  let rec parse_map' curr_map acc lines =
+    match lines with
+    | [] ->
+      let curr_map = { curr_map with ranges = List.rev curr_map.ranges } in
+      curr_map :: acc
+    | line :: lines ->
+      if is_new_map line
+      then (
+        let name = Scanf.sscanf line "%s map:" (fun s -> s) in
+        let curr_map = { name; ranges = [] } in
+        parse_map' curr_map acc lines)
+      else if is_range line
+      then (
+        let destination_range_start = Re.Str.matched_group 1 line |> int_of_string in
+        let source_range_start = Re.Str.matched_group 2 line |> int_of_string in
+        let length = Re.Str.matched_group 3 line |> int_of_string in
+        let range = { destination_range_start; source_range_start; length } in
+        let curr_map = { curr_map with ranges = range :: curr_map.ranges } in
+        parse_map' curr_map acc lines)
+      else if is_empty line
+      then (
+        let curr_map = { curr_map with ranges = List.rev curr_map.ranges } in
+        parse_map' curr_map (curr_map :: acc) lines)
+      else failwith "Unexpected input"
+  in
+  parse_map' { name = ""; ranges = [] } [] input |> List.rev
+;;
+
+let translate_number number ~map =
+  let range =
+    List.find_opt
+      (fun r ->
+        r.source_range_start <= number && number < r.source_range_start + r.length)
+      map.ranges
+  in
+  match range with
+  | None -> number
+  | Some range ->
+    let offset = number - range.source_range_start in
+    range.destination_range_start + offset
+;;
+
+let translate_seed_to_soil seed_nr maps =
+  seed_nr
+  |> translate_number ~map:(List.find (fun m -> m.name = "seed-to-soil") maps)
+  |> translate_number ~map:(List.find (fun m -> m.name = "soil-to-fertilizer") maps)
+  |> translate_number ~map:(List.find (fun m -> m.name = "fertilizer-to-water") maps)
+  |> translate_number ~map:(List.find (fun m -> m.name = "water-to-light") maps)
+  |> translate_number ~map:(List.find (fun m -> m.name = "light-to-temperature") maps)
+  |> translate_number ~map:(List.find (fun m -> m.name = "temperature-to-humidity") maps)
+  |> translate_number ~map:(List.find (fun m -> m.name = "humidity-to-location") maps)
+;;
+
+let parse_input input =
+  let seeds =
+    List.hd input
+    |> String.split_on_char ' '
+    |> List.filter (fun s -> s <> "seeds:")
+    |> List.map int_of_string
+  in
+  let maps = parse_maps (List.tl (List.tl input)) in
+  seeds, maps
+;;
+
+(* takes a starting number and a length and returns a list of all numbers from start to start + length - 1 *)
+let rec range start length =
+  if length = 0 then [] else start :: range (start + 1) (length - 1)
+;;
+
+(* transform list of ints into list of pairs *)
+let rec pairs = function
+  | [] -> []
+  | [ _ ] -> []
+  | x :: y :: rest -> (x, y) :: pairs rest
+;;
+
+let solve_part_one data =
+  let seeds, maps = parse_input data in
+  let soil = List.map (fun seed -> translate_seed_to_soil seed maps) seeds in
+  (* min of the soil numbers *)
+  List.fold_left min (List.hd soil) (List.tl soil)
+;;
+
+let solve_part_two data =
+  let seeds, maps = parse_input data in
+  let pairs = pairs seeds in
+  let ranges = List.map (fun (a, b) -> range a b) pairs in
+  let ranges_flat = List.flatten ranges in
+  let soil = List.map (fun seed -> translate_seed_to_soil seed maps) ranges_flat in
+  (* min of the soil numbers *)
+  List.fold_left min (List.hd soil) (List.tl soil)
+;;
---
+++ test/test_day05.ml
@@ -0,0 +1,155 @@
+open Aoc_2023.Day05
+
+let test_data =
+  [ "seeds: 79 14 55 13"
+  ; ""
+  ; "seed-to-soil map:"
+  ; "50 98 2"
+  ; "52 50 48"
+  ; ""
+  ; "soil-to-fertilizer map:"
+  ; "0 15 37"
+  ; "37 52 2"
+  ; "39 0 15"
+  ; ""
+  ; "fertilizer-to-water map:"
+  ; "49 53 8"
+  ; "0 11 42"
+  ; "42 0 7"
+  ; "57 7 4"
+  ; ""
+  ; "water-to-light map:"
+  ; "88 18 7"
+  ; "18 25 70"
+  ; ""
+  ; "light-to-temperature map:"
+  ; "45 77 23"
+  ; "81 45 19"
+  ; "68 64 13"
+  ; ""
+  ; "temperature-to-humidity map:"
+  ; "0 69 1"
+  ; "1 0 69"
+  ; ""
+  ; "humidity-to-location map:"
+  ; "60 56 37"
+  ; "56 93 4"
+  ]
+;;
+
+let test_parse_maps input expected =
+  let actual = parse_maps input in
+  (* print maps *)
+  List.iter
+    (fun map ->
+      print_endline map.name;
+      List.iter
+        (fun range ->
+          Printf.printf
+            "  dest: %d | src: %d (%d)\n"
+            range.destination_range_start
+            range.source_range_start
+            range.length)
+        map.ranges)
+    actual;
+  (* check maps *)
+  List.iter2
+    (fun expected_map actual_map ->
+      Alcotest.(check string) "same name" expected_map.name actual_map.name;
+      List.iter2
+        (fun expected_range actual_range ->
+          Alcotest.(check int)
+            ("same destination range start in map " ^ expected_map.name)
+            expected_range.destination_range_start
+            actual_range.destination_range_start;
+          Alcotest.(check int)
+            ("same source range start in map " ^ expected_map.name)
+            expected_range.source_range_start
+            actual_range.source_range_start;
+          Alcotest.(check int) "same length" expected_range.length actual_range.length)
+        expected_map.ranges
+        actual_map.ranges)
+    expected
+    actual
+;;
+
+let test_solve_part_one input expected =
+  let actual = solve_part_one input in
+  Alcotest.(check int) "same result" expected actual
+;;
+
+let () =
+  let open Alcotest in
+  run
+    "Day05"
+    [ ( "parse_maps"
+      , [ test_case "parse maps" `Quick (fun () ->
+            let input =
+              [ "seed-to-soil map:"
+              ; "50 98 2"
+              ; "52 50 48"
+              ; ""
+              ; "soil-to-fertilizer map:"
+              ; "0 15 37"
+              ; "37 52 2"
+              ; "39 0 15"
+              ; ""
+              ; "fertilizer-to-water map:"
+              ; "49 53 8"
+              ; "0 11 42"
+              ; "42 0 7"
+              ; "57 7 4"
+              ]
+            in
+            let expected =
+              [ { name = "seed-to-soil"
+                ; ranges =
+                    [ { destination_range_start = 50
+                      ; source_range_start = 98
+                      ; length = 2
+                      }
+                    ; { destination_range_start = 52
+                      ; source_range_start = 50
+                      ; length = 48
+                      }
+                    ]
+                }
+              ; { name = "soil-to-fertilizer"
+                ; ranges =
+                    [ { destination_range_start = 0
+                      ; source_range_start = 15
+                      ; length = 37
+                      }
+                    ; { destination_range_start = 37
+                      ; source_range_start = 52
+                      ; length = 2
+                      }
+                    ; { destination_range_start = 39
+                      ; source_range_start = 0
+                      ; length = 15
+                      }
+                    ]
+                }
+              ; { name = "fertilizer-to-water"
+                ; ranges =
+                    [ { destination_range_start = 49
+                      ; source_range_start = 53
+                      ; length = 8
+                      }
+                    ; { destination_range_start = 0
+                      ; source_range_start = 11
+                      ; length = 42
+                      }
+                    ; { destination_range_start = 42; source_range_start = 0; length = 7 }
+                    ; { destination_range_start = 57; source_range_start = 7; length = 4 }
+                    ]
+                }
+              ]
+            in
+            test_parse_maps input expected)
+        ] )
+    ; ( "solve_part_one"
+      , [ test_case "solve part one" `Quick (fun () -> test_solve_part_one test_data 35) ]
+      )
+    ]
+;;
