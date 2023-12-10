925e268ed95b1e775ee6792d43ecfe2914b0b556
Rascat <rascat@mailbox.org>
2023-12-10 22:13:17 +0100

    Define CLI with Cmdliner

--- bin/main.ml
+++ bin/main.ml
@@ -1,27 +1,56 @@
-let () =
-  let d1_data = Aoc_2023.Utils.read_lines "inputs/day01.txt" in
-  let d2_data = Aoc_2023.Utils.read_lines "inputs/day02.txt" in
-  let d3_data = Aoc_2023.Utils.read_lines "inputs/day03.txt" in
-  let d4_data = Aoc_2023.Utils.read_lines "inputs/day04.txt" in
-  let d5_data = Aoc_2023.Utils.read_lines "inputs/day05.txt" in
-  let d6_data = Aoc_2023.Utils.read_lines "inputs/day06.txt" in
-  let d7_data = Aoc_2023.Utils.read_lines "inputs/day07.txt" in
-  let d8_data = Aoc_2023.Utils.read_lines "inputs/day08.txt" in
-  let d9_data = Aoc_2023.Utils.read_lines "inputs/day09.txt" in
-  Printf.printf "Solution to Day 1.1: %i\n" (Aoc_2023.Day01.solve_part_one d1_data);
-  Printf.printf "Solution to Day 1.2: %i\n" (Aoc_2023.Day01.solve_part_two d1_data);
-  Printf.printf "Solution to Day 2.1: %i\n" (Aoc_2023.Day02.solve_part_one d2_data);
-  Printf.printf "Solution to Day 2.2: %i\n" (Aoc_2023.Day02.solve_part_two d2_data);
-  Printf.printf "Solution to Day 3.1: %i\n" (Aoc_2023.Day03.solve_part_one d3_data);
-  Printf.printf "Solution to Day 4.1: %i\n" (Aoc_2023.Day04.solve_part_one d4_data);
-  Printf.printf "Solution to Day 4.2: %i\n" (Aoc_2023.Day04.solve_part_two d4_data);
-  Printf.printf "Solution to Day 5.1: %i\n" (Aoc_2023.Day05.solve_part_one d5_data);
-  Printf.printf "Solution to Day 5.2: %i\n" (Aoc_2023.Day05.solve_part_two_seq d5_data);
-  Printf.printf "Solution to Day 6.1: %i\n" (Aoc_2023.Day06.solve_part_one d6_data);
-  Printf.printf "Solution to Day 6.2: %i\n" (Aoc_2023.Day06.solve_part_two d6_data);
-  Printf.printf "Solution to Day 7.1: %i\n" (Aoc_2023.Day07.solve_part_one d7_data);
-  Printf.printf "Solution to Day 8.1: %i\n" (Aoc_2023.Day08.solve_part_one d8_data);
-  (* Printf.printf "Solution to Day 8.2: %i\n" (Aoc_2023.Day08.solve_part_two d8_data) *)
-  Printf.printf "Solution to Day 9.1: %i\n" (Aoc_2023.Day09.solve_part_one d9_data);
-  Printf.printf "Solution to Day 9.2: %i\n" (Aoc_2023.Day09.solve_part_two d9_data)
+open Cmdliner
+
+let aoc_2023 day part file =
+  let data = Aoc_2023.Utils.read_lines file in
+  let solver =
+    match day, part with
+    | 1, 1 -> Some Aoc_2023.Day01.solve_part_one
+    | 1, 2 -> Some Aoc_2023.Day01.solve_part_two
+    | 2, 1 -> Some Aoc_2023.Day02.solve_part_one
+    | 2, 2 -> Some Aoc_2023.Day02.solve_part_two
+    | 3, 1 -> Some Aoc_2023.Day03.solve_part_one
+    | 3, 2 -> Some Aoc_2023.Day03.solve_part_two
+    | 4, 1 -> Some Aoc_2023.Day04.solve_part_one
+    | 4, 2 -> Some Aoc_2023.Day04.solve_part_two
+    | 5, 1 -> Some Aoc_2023.Day05.solve_part_one
+    | 5, 2 -> Some Aoc_2023.Day05.solve_part_two
+    | 6, 1 -> Some Aoc_2023.Day06.solve_part_one
+    | 6, 2 -> Some Aoc_2023.Day06.solve_part_two
+    | 7, 1 -> Some Aoc_2023.Day07.solve_part_one
+    | 7, 2 -> Some Aoc_2023.Day07.solve_part_two
+    | 8, 1 -> Some Aoc_2023.Day08.solve_part_one
+    | 8, 2 -> Some Aoc_2023.Day08.solve_part_two
+    | 9, 1 -> Some Aoc_2023.Day09.solve_part_one
+    | 9, 2 -> Some Aoc_2023.Day09.solve_part_two
+    | _ -> None
+  in
+  match solver with
+  | Some fn -> print_endline (Printf.sprintf "Day %d.%d: %d" day part (fn data))
+  | None -> print_endline (Printf.sprintf "No solution for day %d.%d" day part)
 ;;
+
+let day =
+  let doc = "Specifies the day for which the solution is computed (1-24)." in
+  Arg.(required & pos 0 (some int) None & info [] ~docv:"DAY" ~doc)
+;;
+
+let part =
+  let doc = "Specifies the part for which the solution is computed (1 or 2)." in
+  Arg.(required & pos 1 (some int) None & info [] ~docv:"PART" ~doc)
+;;
+
+let file =
+  let doc = "Specifies the path to the input file" in
+  Arg.(required & pos 2 (some string) None & info [] ~docv:"FILE" ~doc)
+;;
+
+let solve_aoc_t = Term.(const aoc_2023 $ day $ part $ file)
+
+let cmd =
+  let doc = "compute solution for any of 2023's Advent of Code challenges" in
+  let info = Cmd.info "aoc_2023" ~doc in
+  Cmd.v info solve_aoc_t
+;;
+
+let main () = exit (Cmd.eval cmd)
+let () = main ()
